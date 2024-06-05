38e01dd1a74605c2ed94fa9e5d442d86a69b5aa2
StepanMkr <stepan.weightlifting@gmail.com>
2024-06-06 00:23:32 +0500

    cards left

--- js/script.js
+++ js/script.js
@@ -7,23 +7,45 @@ for (let article of articles) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Находим кнопку "Подробнее" внутри.blog-article-2
     const moreButton = document.querySelector('.more');
-
-    // Добавляем обработчик события клика для кнопки
     moreButton.addEventListener('click', function() {
-        // Скрываем кнопку
-        this.style.display = 'none';
-
-        // Создаем новый параграф
         const newParagraph = document.createElement('p');
         newParagraph.classList.add('blog-article-description');
         newParagraph.textContent = 'Станьте свидетелем волнительных моментов презентации, где молодые предприниматели с пылом и вдохновением представляют свои проекты перед ведущими экспертами и инвесторами.';
-
-        // Находим родительский элемент.blog-article-content внутри.blog-article-2
+        newParagraph.style.marginBottom = '0';
         const articleContent = this.closest('.blog-article-content');
-
-        // Добавляем новый параграф в конец.blog-article-content
         articleContent.appendChild(newParagraph);
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const moreButton = document.querySelector('.more-blog-article-2');
+    moreButton.addEventListener('click', function() {
+        const newParagraph = document.createElement('p');
+        newParagraph.classList.add('blog-article-description');
+        newParagraph.textContent = 'В атмосфере непринуждённого общения вы сможете пообщаться с единомышленниками, узнать о новых проектах и вакансиях, а также найти партнёров и инвесторов для своих идей.'; // Содержимое нового параграфа
+        newParagraph.style.marginBottom = '0';
+        const articleContent = this.closest('.blog-article-content');
+        const lastParagraph = articleContent.lastElementChild;
+        articleContent.insertBefore(newParagraph, lastParagraph);
+        const targetParagraph = document.querySelector('.blog-article-3.blog-article-description:last-child');
+        targetParagraph.insertAdjacentHTML('afterend', '<p class="blog-article-description">Пока</p>');
+    });
+});
+
+
+
+let previewList = document.querySelector('.preview-list');
+let images = document.querySelectorAll('.preview-list a');
+let mainPhoto = document.querySelector('.active-photo');
+
+for (let image of images) {
+    image.onclick = function(evt) {
+        evt.preventDefault();
+        mainPhoto.src = image.href;
+        let currentImage = document.querySelector('.active-item');
+        currentImage.classList.remove('active-item');
+        image.classList.add('active-item');
+    }
+}
